6d49328474101d610bf79ab928801594ff505f80
Danny O'Sullivan <dosullivan557@gmail.com>
2025-07-15 23:02:50 +0100

    Avatar icon

---
+++ src/components/atoms/avatar/Avatar.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Avatar from './Avatar';
+const meta: Meta<typeof Avatar> = {
+  title: 'Atoms/Avatar',
+  component: Avatar,
+  argTypes: {
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  args: {
+    name: 'Default Avatar'
+  }
+};
+
+export const WithCustomClass: Story = {
+  args: {
+    name: 'Custom Class Avatar'
+  }
+};
+
+export const SingleWord: Story = {
+  args: {
+    name: 'Danny'
+  }
+};
